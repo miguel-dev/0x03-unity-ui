54ef13a8bba65a18e4c6f31227da073d00b40ab8
miguel-dev <miguelacortes@zoho.com>
2021-01-09 11:15:16 -0500

    [Fixes] When the colorblind option is off, the trap and goal materials goes back to their original state

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -25,13 +25,19 @@ public class MainMenu : MonoBehaviour
 
     public void PlayMaze()
     {
-        if (colorblindMode.isOn)
+        if (colorblindMode.isOn == true)
         {
             trapMat.color = new Color32(255, 112, 0, 1);
             goalMat.color = Color.blue;
         }
+        else
+        {
+            trapMat.color = Color.red;
+            goalMat.color = Color.green;
+        }
         SceneManager.LoadScene("maze");
     }
+    
     public void QuitMaze()
     {
         Application.Quit();
