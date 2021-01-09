95902e1ca8ece0151c03b576fc3fefdb88063858
miguel-dev <miguelacortes@zoho.com>
2021-01-08 20:28:27 -0500

    [Task] 10. We're not retreating, we're advancing! Toward future victory!

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -24,4 +24,10 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadScene("maze");
     }
+    public void QuitMaze()
+    {
+        Application.Quit();
+        Debug.Log("Quit Game");
+    }
+    
 }
